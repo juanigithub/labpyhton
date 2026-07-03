bc8f0cb8756bbbc24e3120b01f907cce7a81c291
valentina Alvira <alviravalentina38@gmail.com>
2026-07-03 18:16:05 -0300

    implementacion de ID para cada curso

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 
 #se puso max 3 para que sea mas facil de hacer las pruebas 
 cursos = [
-    {"nombre": "Python Basico", "cupo_max": 3, "inscriptos": [], "lista_espera": []},
-    {"nombre": "Excel Avanzado", "cupo_max": 2, "inscriptos": [], "lista_espera": []},
-    {"nombre": "Diseño Web", "cupo_max": 3, "inscriptos": [], "lista_espera": []},
+    {"id": 1, "nombre": "Python Básico", "cupo_max": 3, "inscriptos": [], "lista_espera": []},
+    {"id": 2, "nombre": "Excel Avanzado", "cupo_max": 2, "inscriptos": [], "lista_espera": []},
+    {"id": 3, "nombre": "Diseño Web", "cupo_max": 3, "inscriptos": [], "lista_espera": []},
 ]
 
 #funcion mostrar menu 
@@ -23,22 +23,24 @@ def ver_cursos(cursos):
         inscriptos_actual = len(curso["inscriptos"])
         cupo_disponible = curso["cupo_max"] - inscriptos_actual
         en_espera = len(curso["lista_espera"])
-        print(f"{curso['nombre']} | Inscriptos: {inscriptos_actual}/{curso['cupo_max']} | Cupos disponibles: {cupo_disponible} | En lista de espera: {en_espera}")
+        print(f"ID: {curso['id']} | {curso['nombre']} | Inscriptos: {inscriptos_actual}/{curso['cupo_max']} | Cupos disponibles: {cupo_disponible} | En lista de espera: {en_espera}")
 
 #funcion inscribir estudiantes (el nombre tiene escribirse tal y como esta o no lo tomara)
 def inscribir_estudiante(cursos):
     ver_cursos(cursos)
-    nombre_curso = input("¿A qué curso desea inscribirse? ")
+    try:
+        id_curso = int(input("Ingrese el ID del curso: "))
+    except ValueError:
+        print("Debe ingresar un número válido.")
+        return
+
     nombre_estudiante = input("Nombre del estudiante: ")
-    #validacion de que el nombre no este vacio strip rechaza el caso en que el usuario ponga solo espacios en blanco (" ") como si fuera un nombre.
     while nombre_estudiante.strip() == "":
         print("El nombre no puede estar vacío.")
         nombre_estudiante = input("Nombre del estudiante: ")
-    
+
     for curso in cursos:
-        #lower normaliza ambos lados en minusculas para que no haya problemas al comparar.
-        if curso["nombre"].lower() == nombre_curso.lower():
-            #mientras haya menos inscriptos que el cupo máximo, entra en el curso normal.
+        if curso["id"] == id_curso:
             if len(curso["inscriptos"]) < curso["cupo_max"]:
                 curso["inscriptos"].append({"nombre": nombre_estudiante})
                 print(f"{nombre_estudiante} inscripto en {curso['nombre']}")
@@ -46,7 +48,7 @@ def inscribir_estudiante(cursos):
                 curso["lista_espera"].append({"nombre": nombre_estudiante})
                 print(f"El curso {curso['nombre']} está lleno. {nombre_estudiante} fue agregado a la lista de espera.")
             return
-    print("Curso no encontrado")
+    print("No existe un curso con ese ID.")
 
 #rama de ejecucion del programa
 while True:
