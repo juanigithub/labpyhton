99ee92f52e10388312d766bc96dc92a6a5e2ca68
valentina Alvira <alviravalentina38@gmail.com>
2026-07-03 15:19:51 -0300

    defino estructura inicial de cursos

---
+++ main.py
@@ -0,0 +1,7 @@
+
+cursos = [
+    {"nombre": "Python Básico", "cupo_max": 3, "inscriptos": [], "lista_espera": []},
+    {"nombre": "Excel Avanzado", "cupo_max": 2, "inscriptos": [], "lista_espera": []},
+    {"nombre": "Diseño Web", "cupo_max": 3, "inscriptos": [], "lista_espera": []},
+]
+
