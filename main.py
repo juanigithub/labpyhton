af9e012d07231a29228bed4e4eca436098ab1d78
valentina Alvira <alviravalentina38@gmail.com>
2026-07-03 23:18:20 -0300

    encapsula el menú principal en la función main() eh implementacion de Id para cada alumno (DNI)

--- main.py
+++ main.py
@@ -39,17 +39,33 @@ def inscribir_estudiante(cursos):
         print("El nombre no puede estar vacío.")
         nombre_estudiante = input("Nombre del estudiante: ")
 
+    # Repetir hasta que el DNI sea un número válido
+    dni_valido = False
+    while not dni_valido:
+        try:
+            dni_estudiante = int(input("ingrese DNI del estudiante sin puntos ni espacios: "))
+            dni_valido = True
+        except ValueError:
+            print("El DNI debe ser un número válido, respete lo pedido. Intente nuevamente.")
     for curso in cursos:
         if curso["id"] == id_curso:
+            
+            # Chequeamos si ese DNI ya está inscripto o en lista de espera en ESTE curso
+            ya_inscripto = any(estudiante["dni"] == dni_estudiante for estudiante in curso["inscriptos"])
+            ya_en_espera = any(estudiante["dni"] == dni_estudiante for estudiante in curso["lista_espera"])
+
+            if ya_inscripto or ya_en_espera:
+                print(f"El DNI {dni_estudiante} ya está registrado en el curso {curso['nombre']}.")
+                return
+
             if len(curso["inscriptos"]) < curso["cupo_max"]:
-                curso["inscriptos"].append({"nombre": nombre_estudiante})
+                curso["inscriptos"].append({"nombre": nombre_estudiante, "dni": dni_estudiante})
                 print(f"{nombre_estudiante} inscripto en {curso['nombre']}")
             else:
-                curso["lista_espera"].append({"nombre": nombre_estudiante})
+                curso["lista_espera"].append({"nombre": nombre_estudiante, "dni": dni_estudiante})
                 print(f"El curso {curso['nombre']} está lleno. {nombre_estudiante} fue agregado a la lista de espera.")
             return
     print("No existe un curso con ese ID.")
-
 
 #funcion ver estadisticas 
 def ver_estadisticas(cursos):
@@ -91,19 +107,26 @@ def ver_estadisticas(cursos):
     print("\n> Curso con mayor demanda:")
     print(f"{curso_mas_demanda} con {max_demanda} personas interesadas (inscriptos + lista de espera)")
 
+
+def main():
+    while True:
+        mostrar_menu()
+        opcion = input("seleccione una opción: ")
+        if opcion == "0":
+            print("¡Hasta luego!")
+            break 
+        elif opcion == "1":
+            inscribir_estudiante(cursos)
+        elif opcion == "2":
+            ver_cursos(cursos)  
+        elif opcion == "3":
+            ver_estadisticas(cursos)
+        else:
+            print("opcion invalida, por favor ingrese un numero del 0 al 3")
+
+
 #************************************************************************
 #rama de ejecucion del programa
-while True:
-    mostrar_menu()
-    opcion = input("seleccione una opción: ")
-    if opcion == "0":
-        print("¡Hasta luego!")
-        break 
-    elif opcion == "1":
-        inscribir_estudiante(cursos)
-    elif opcion == "2":
-        ver_cursos(cursos)  
-    elif opcion == "3":
-        ver_estadisticas(cursos)
-    else:
-        print("Función aún no implementada")
+
+if __name__ == "__main__":
+    main()
